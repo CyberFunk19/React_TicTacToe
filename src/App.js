6ef108d4201d6dbe1f5ad769038269afe0e86075
Elad Rotfus <117649972+CyberFunk19@users.noreply.github.com>
2022-12-29 12:56:23 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TicTacToe from './TicTacToe/TicTacToe'
+
+const App = () => {
+  return (
+    <div>
+
+      <TicTacToe></TicTacToe>
+
+    </div>
+  )
+}
+
+export default App
